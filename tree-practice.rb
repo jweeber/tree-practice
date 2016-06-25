26127ea62631460669cf8ae16b23d932219ea4ff
Jessica <jessica.weeber@gmail.com>
2016-06-25 11:56:00 -0700

    adding method that prints out three of the expression trees

--- tree-practice.rb
+++ tree-practice.rb
@@ -8,14 +8,41 @@ class TreeNode
    end
 end
 
-def print_infix(node)
+# def print_infix(node)
+#   return if node == nil
+#   print_infix(node.left)
+#   print node.value + " "
+#   print_infix(node.right)
+# end
+
+def operators(node)
   return if node == nil
-  print_infix(node.left)
-  print node.value + " "
-  print_infix(node.right)
+  if node.value.match(/\+|\-|\/|\%|\*/)
+    print node.value
+  end
 end
 
 root = TreeNode.new("+")
 root.left = TreeNode.new("3")
 root.right = TreeNode.new("2")
-print_infix(root)
+operators(root)
+
+puts "\n\n"
+
+second = TreeNode.new("-")
+second.right = TreeNode.new("10")
+second.left = TreeNode.new("+")
+second.left.right = TreeNode.new("2")
+second.left.left = TreeNode.new("3")
+operators(second)
+
+puts "\n"
+
+third = TreeNode.new("+")
+third.right = TreeNode.new("2")
+third.left = TreeNode.new("*")
+third.left.right = TreeNode.new("3")
+third.left.left = TreeNode.new("4")
+operators(third)
+
+puts "\n"
